e061223952e3721b9f9ed2e740dbc53062c79bd7
valerrrich <161199056+valerrrich@users.noreply.github.com>
2024-03-09 18:55:24 +0100

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,14 @@
+    data = generate_random_data()
+
+    for item in data:
+import random
+def generate_random_data():
+
+    return data
+        print(f"Random Number: {item}")
+    main()
+if __name__ == "__main__":
+    data = [random.randint(1, 100) for _ in range(10)]
+
+
+def main():
